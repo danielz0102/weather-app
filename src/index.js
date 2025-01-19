2e9f50bfe301dd3bb2d9d85a74f66607295f81e6
danielz0102 <danyzglez@hotmail.com>
2025-01-19 14:37:13 -0600

    feat: get data from api and log it

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import '@/index.css'
+
+const data = await getWeatherData('Nuevo Leon,Mexico')
+const weather = processData(data)
+console.log({ weather })
+
+async function getWeatherData(location) {
+  try {
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=K3PYDAWZMZ3FRD6XP4PSXRW6S`
+    const response = await fetch(url)
+
+    if (!response.ok) {
+      throw new Error(response.status)
+    }
+
+    return await response.json()
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+function processData(data) {
+  const { resolvedAddress, description, currentConditions, days } = data
+  return { resolvedAddress, description, currentConditions, days }
+}
