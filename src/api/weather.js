aa36e16e0491856ffb65a5f35fa5169ca8189012
danielz0102 <danyzglez@hotmail.com>
2025-01-20 00:44:05 -0600

    fix(weather.js): return error if fetching fails

--- src/api/weather.js
+++ src/api/weather.js
@@ -2,24 +2,23 @@ export async function getWeather(location) {
   const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=K3PYDAWZMZ3FRD6XP4PSXRW6S`
   const data = await getData(url)
 
-  console.log({ data })
+  if (data instanceof Error) return data
 
   const { resolvedAddress, description, currentConditions, days } = data
-
   return { resolvedAddress, description, currentConditions, days }
 }
 
 async function getData(url) {
   try {
     const response = await fetch(url)
-    console.log(response)
 
     if (!response.ok) {
-      throw new Error(response.status)
+      throw new Error(`HTTP ${response.status}`)
     }
 
     return await response.json()
   } catch (error) {
     console.error(error)
+    return error
   }
 }
